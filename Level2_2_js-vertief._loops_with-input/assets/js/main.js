e3c3db2031b6eb64763312ae06bd3fbc93385a66
janine7492 <janine.supercode@gmail.com>
2022-11-11 09:27:42 +0100

    small update in description

--- Level2_2_js-vertief._loops_with-input/assets/js/main.js
+++ Level2_2_js-vertief._loops_with-input/assets/js/main.js
@@ -1,16 +1,20 @@
 
 
-// let loopArray = [];
+let loopArray = [];
 
 function loopMe() {
+    // User-Eingabe abfragen
     const eingabe = Number(document.getElementById("eingabe").value);
     console.log("input: " + eingabe);
+    // Reset Array vor neuer Ausführung
     loopArray = [];
     document.getElementById("output").innerHTML = "";
 
+    // O-Loop und push in das Array
     for (let i = 1; i <= eingabe; i++) {
         loopArray.push("o");
     }
 
+    // Ausgabe des Textes mit geloopten Os
     document.getElementById("output").innerHTML = `L${loopArray.join("")}p`;
 }
